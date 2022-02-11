f81dab59ea629d480b5d33226731319d53e52672
BhagyashreeDhamande <99469329+BhagyashreeDhamande@users.noreply.github.com>
2022-02-11 18:27:37 +0530

    Update demo.java

--- demo12/src/demo12/com/demo.java
+++ demo12/src/demo12/com/demo.java
@@ -3,7 +3,11 @@ package demo12.com;
 public class demo {
 public static void main(String[] args) {
 	System.out.println("hiiii");
-	System.out.println("new updt on git");
-	
+	System.out.println("new updt on git");	
 }
+	public void m1()
+	{
+	System.out.println("method m1 added");	
+	}
+	
 }
